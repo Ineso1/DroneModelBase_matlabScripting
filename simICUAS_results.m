2a5c8b60cf98b9ae2962247fe4786b0028299152
ateverz <alejandro.tevera@cinvestav.mx>
2025-02-07 09:24:43 +0100

    Numerical comparison of translational and rotational Full UDE for ICUAS

--- simICUAS_results.m
+++ simICUAS_results.m
@@ -18,7 +18,7 @@ iterations = sim_time / dt;
 pid_tht = [2 1 0];
 pid_trq = [40 8 0];
 
-print_settings.save = false;
+print_settings.save = true;
 print_settings.path = './pictures/';
 print_settings.code = '_sim';
 
@@ -38,16 +38,27 @@ normal.disturbanceRejection_rot = diag([0,0,0]);
 pid.disturbanceRejection_trans = diag([0,0,0]);
 pid.disturbanceRejection_rot = diag([0,0,0]);
 
-% UDE
-ude = Drone(mass, q, x0, y0, z0, dt);
-ude.obs_num = 1;
-ude.name = 'UDE';
-ude = ude.setControlGains(pid_tht(1), pid_tht(2), pid_tht(3), pid_trq(1), pid_trq(2), pid_trq(3));
-ude = ude.setAimPoint(1,2,2);
-ude = ude.setDisturbance([0; 0; 0], [0; 0; 0]);
+% UDE rot + trans
+rot = Drone(mass, q, x0, y0, z0, dt);
+rot.obs_num = 1;
+rot.name = 'Full UDE';
+rot = rot.setControlGains(pid_tht(1), pid_tht(2), pid_tht(3), pid_trq(1), pid_trq(2), pid_trq(3));
+rot = rot.setAimPoint(1,2,2);
+rot = rot.setDisturbance([0; 0; 0], [0; 0; 0]);
 
-ude.disturbanceRejection_trans = diag([0,0,0]);
-ude.disturbanceRejection_rot = diag([0,0,0]);
+rot.disturbanceRejection_trans = diag([0,0,0]);
+rot.disturbanceRejection_rot = diag([0,0,0]);
+
+% UDE (only translational)
+trans = Drone(mass, q, x0, y0, z0, dt);
+trans.obs_num = 1;
+trans.name = 'Translational UDE';
+trans = trans.setControlGains(pid_tht(1), pid_tht(2), pid_tht(3), pid_trq(1), pid_trq(2), pid_trq(3));
+trans = trans.setAimPoint(1,2,2);
+trans = trans.setDisturbance([0; 0; 0], [0; 0; 0]);
+
+trans.disturbanceRejection_trans = diag([0,0,0]);
+trans.disturbanceRejection_rot = diag([0,0,0]);
 
 % Luenberger
 luenberger = Drone(mass, q, x0, y0, z0, dt);
@@ -68,37 +79,43 @@ for i = 1:iterations
     % se me hace un poco mal tener que poner el observador "justo antes" de
     % la perturbación. 
     if time >= 10  
-        ude.disturbanceRejection_trans = diag([1,1,1]);
-        ude.disturbanceRejection_rot = diag([15,15,15]);
+        rot.disturbanceRejection_trans = diag([1,1,1]);
+        rot.disturbanceRejection_rot = diag([15,15,15]);
+        
+        trans.disturbanceRejection_trans = 0.55*diag([1,1,1]);
 
-        luenberger.disturbanceRejection_trans = 1*diag([1,1,1]);
+        luenberger.disturbanceRejection_trans = 0.5*diag([1,1,1]);
         luenberger.disturbanceRejection_rot = 2*diag([1,1,1]);
     end
 
     if time >= 10
         w = 0.5; % En el paper es 0.1, por eso se ve descente. Idealmente w = 0.5 o más :)
-        %disturbance_trans = [1;2;5];
-        disturbance_trans = [1 * sin(w * time); 2 * cos(w*time); 5 * sin(w*time)];
-        disturbance_rot = [0; 0; 0];
+        %disturbance_trans = [1;1;2];
+        disturbance_trans = [1 * sin(w * time); 1 * cos(w*time); 2 * sin(w*time)];
+        a = 1e-2; b = 0.5;
+        disturbance_rot = [0; 0; 0]; %[a*sin(b*w*time); a*sin(b*w*time); a*sin(b*w*time)];
 
     else
         disturbance_trans = [0;0;0];
         disturbance_rot = [0; 0; 0];
     end
 
-    ude = ude.setDisturbance(disturbance_trans, disturbance_rot);
+    rot = rot.setDisturbance(disturbance_trans, disturbance_rot);
+    trans = trans.setDisturbance(disturbance_trans, disturbance_rot);
     normal = normal.setDisturbance(disturbance_trans, disturbance_rot);
     luenberger = luenberger.setDisturbance(disturbance_trans, disturbance_rot);
     
-    ude = ude.update();
+    rot = rot.update();
+    trans = trans.update();
     normal = normal.update();
     luenberger = luenberger.update();
 end
 
 %% Comparison
+close all
 % Set colors for each simulation and build the array for comparison. 
-colors = ["#D81E5B", "#22577A","#5E8C61"];
-simulations = [normal, ude, luenberger];
+colors = ["#D81E5B","#5E8C61","#02dcc8","#22577A",];
+simulations = [normal, luenberger, trans, rot];
 
 % Compute evaluation and show results. 
 results = ResultsAnalysis(simulations, colors , print_settings);
